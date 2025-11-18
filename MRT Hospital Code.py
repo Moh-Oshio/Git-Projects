d607b9739cc2e25404dd6b4a94b696fef46836bb
Mohammed Yahayah <mohammedyahayah@gmail.com>
2025-11-18 21:34:03 +0100

    This is another random commit

--- MRT Hospital Code.py
+++ MRT Hospital Code.py
@@ -1,2 +1,11 @@
 # This is my second project
 # In this project, I want to build a simple CLI application that a hospital can use to create, edit and view patients' records.
+
+
+# Import some modules that would be useful for the CLI app.
+
+# The randint will be used to generate a random patient number everytime a new patient seeks treatment
+from random import randint
+
+#
+from datetime import datetime, date
