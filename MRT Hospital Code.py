11b23f19843c21c871610878e6d61fb391ae9d99
Mohammed Yahayah <mohammedyahayah@gmail.com>
2025-11-20 21:24:27 +0100

    Imported the modules that I might need in the Hospital CLI Project.

--- MRT Hospital Code.py
+++ MRT Hospital Code.py
@@ -7,5 +7,8 @@
 # The randint will be used to generate a random patient number everytime a new patient seeks treatment
 from random import randint
 
-#
+# The datetime module will be used to track the time a patient has started treament while in the hospital
 from datetime import datetime, date
+
+# This is a list to store all the card numbers of each patient. It will be trasversed from time to time.
+cards_num = []
